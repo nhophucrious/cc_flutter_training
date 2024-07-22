c3330d1557c643e8392a646d69dd9d5c8b15dcbd
Mac mini <macmini@Phuc-Nho-Station.local>
2024-07-22 13:29:20 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/cc_flutter_training/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cc_flutter_training
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage()
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Training - CC',),
+        centerTitle: true,
+        backgroundColor: Colors.amber[200],
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+               Text(
+                'Flutter Training by Phuc Nguyen',
+                style: TextStyle(fontSize: 20),
+              ),
+               SizedBox(height: 20),
+             
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
